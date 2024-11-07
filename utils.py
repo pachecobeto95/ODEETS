bfe309cbb62313fb20959db71b1aa78443488c7b
Roberto <robertopacheco@id.uff.br>
2024-11-07 14:46:17 -0300

    eval global ts

--- utils.py
+++ utils.py
@@ -75,33 +75,3 @@ def load_caltech256(args, dataset_path, indices_path):
 	test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, num_workers=4, pin_memory=True)
 
 	return train_loader, val_loader, test_loader
-
-
-class ECE(nn.Module):
-	"""This method computes ECE metric to measure model's miscalibration"""
-
-	def __init__(self, n_bins=15):
-		"""
-		n_bins (int): number of confidence interval bins
-		"""
-		super(ECE, self).__init__()
-		bin_boundaries = torch.linspace(0, 1, n_bins + 1)
-		self.bin_lowers = bin_boundaries[:-1]
-		self.bin_uppers = bin_boundaries[1:]
-
-	def forward(self, logits, labels):
-		softmaxes = F.softmax(logits, dim=1)
-		confidences, predictions = torch.max(softmaxes, 1)
-		accuracies = predictions.eq(labels)
-
-		ece = torch.zeros(1, device=logits.device)
-		for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
-			# Calculated |confidence - accuracy| in each bin
-			in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
-			prop_in_bin = in_bin.float().mean()
-			if (prop_in_bin.item() > 0):
-				accuracy_in_bin = accuracies[in_bin].float().mean()
-				avg_confidence_in_bin = confidences[in_bin].mean()
-				ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
-
-		return ece
