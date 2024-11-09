e19a533df8bfc57b0a76e2688ac2e196c789282e
Roberto <robertopacheco@id.uff.br>
2024-11-09 14:10:21 -0300

    eval global ts

--- eval_inference_global_TS.py
+++ eval_inference_global_TS.py
@@ -6,6 +6,65 @@ import torch.optim as optim
 import torch.nn as nn
 import pandas as pd
 
+def run_ee_dnn_inference(args, model, inf_time_data, test_loader, threshold, overhead, device):
+
+	correct_list, inf_time_list, isOffloaded_list, exit_branch_list, correct_edge_list = [], [], [], [], []
+
+	model.eval()
+	
+	with torch.no_grad():
+		for (data, target) in tqdm(test_loader):	
+
+			# Convert data and target into the current device.
+			data, target = data.to(device), target.to(device)
+
+			# Obtain confs and predictions for each side branch.
+			prediction, isOffloaded, exit_branch = model.forwardGlobalTSInference(data, threshold)
+
+			correct = prediction.eq(target.view_as(prediction)).sum().item()
+			inf_time = compute_inference_time(inf_time_data, exit_branch, args.n_branches, overhead)
+			
+
+			inf_time_list.append(inf_time), isOffloaded_list.append(isOffloaded), correct_list.append(correct), 
+
+			if(isOffloaded):
+				correct_edge_list.append(correct)
+
+
+	correct_list, inf_time_list, isOffloaded_list = np.array(correct_list), np.array(inf_time_list), np.array(isOffloaded_list)
+
+
+	overall_accuracy = sum(correct_list)/len(correct_list)
+	accuracy_edge = sum(correct_edge_list)/len(correct_edge_list)
+	offloading_prob = sum(isOffloaded_list)/len(isOffloaded_list)
+
+	
+	result_dict = {"accuracy_edge": accuracy_edge, "offloading_prob": offloading_prob, "overall_accuracy": overall_accuracy, 
+	"avg_inf_time": , "std_inf_time": , "threshold": threshold, "overhead": overhead}
+
+	temp_data = model.get_temperature_data()
+
+	result_dict.update(temp_data)
+
+
+	sys.exit()
+
+
+	#Converts to a DataFrame Format.
+	df = pd.DataFrame(np.array(list(result_dict.values())).T, columns=list(result_dict.keys()))
+
+	# Returns confidences and predictions into a DataFrame.
+	return df
+
+
+def get_inference_time(df_cloud, df_edge):
+
+	print(df_cloud)
+
+	print(df_edge)
+
+	sys.exit()
+
 
 def main(args):
 
@@ -21,10 +80,14 @@ def main(args):
 	indices_path = os.path.join(config.DIR_PATH, "indices_%s.pt"%(args.dataset_name))
 
 	inf_data_dir_path = os.path.join(config.DIR_PATH, args.model_name, "inference_data")
-	os.makedirs(inf_data_dir_path, exist_ok=True)
-
-	inf_data_path = os.path.join(inf_data_dir_path, "global_TS_inf_data_ee_%s_%s_branches_%s_id_%s.csv"%(args.model_name, 
+	
+	inf_data_path = os.path.join(inf_data_dir_path, "inf_data_ee_%s_%s_branches_%s_id_%s.csv"%(args.model_name, 
 		args.n_branches, args.loss_weights_type, args.model_id))
+
+	edge_inf_data_path = os.path.join(inf_data_dir_path, "inference_data_mobilenet_1_branches_1_jetson_nano.csv")
+
+	result_path = os.path.join(config.DIR_PATH, args.model_name, "results", "globalTS")
+	os.makedirs(result_path, exist_ok=True)
 	
 	ee_model = ee_dnns.load_eednn_model(args, n_classes, model_path, device)
 
@@ -32,15 +95,17 @@ def main(args):
 
 	_, val_loader, test_loader = utils.load_caltech256(args, dataset_path, indices_path)
 
+	df_cloud, df_edge = pd.read_csv(inf_data_path), pd.read_csv(edge_inf_data_path)
 
-	threshold_list = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
-	temp_init = 1.0
-	max_iter = 1000
+	inf_time_cloud, inf_time_edge = get_inference_time(df_cloud, df_edge)
 
-	for threshold in threshold_list:
+	for threshold in config.threshold_list:
 		print("Threshold: %s"%(threshold))
-		global_ts_calib_model = ts.GlobalTemperatureScaling(ee_model, device, temp_init, max_iter, args.n_branches, threshold)
+		global_ts_calib_model = ts.GlobalTemperatureScaling(ee_model, device, args.temp_init, args.max_iter, args.n_branches, threshold)
 		global_ts_calib_model.run(val_loader)
+
+		for overhead in config.overhead_list:
+			inf_result = run_ee_dnn_inference(global_ts_calib_model, test_loader, threshold, overhead, device)
 
 
 
@@ -91,6 +156,11 @@ if (__name__ == "__main__"):
 		help='Train Batch Size. Default: %s'%(config.batch_size_train))
 
 	parser.add_argument('--location', type=str, help='Which machine extracts the inference data', choices=["pechincha", "jetson", "RO"])
+
+
+	parser.add_argument('--temp_init', type=float, default=1.0, help='Initial temperature to start the Temperature Scaling')
+
+	parser.add_argument('--max_iter', type=float, default=1000, help='Max Interations to optimize the Temperature Scaling')
 
 	args = parser.parse_args()
 
